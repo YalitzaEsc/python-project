71c7cf4335362cab61d91dd633d8ff391b952b69
YalitzaEsc <L20170702@culiacan.tecnm.mx>
2024-03-17 13:47:23 -0700

    pc

--- Python/DiaUno/Input.py
+++ Python/DiaUno/Input.py
@@ -1,3 +1,3 @@
 print("\nTu nombre completo es: " + input("Dime tu nombre: ") + " " + input("Dime tu apellido: ") + "\nTu carrera es: " + input("¿Qué estudias? "))
-print("El país en el que vives es: " + input("¿En qué país vives? "))
+print("El país en el que vives es: " + input("¿En qué país vives actualmente? "))
 
